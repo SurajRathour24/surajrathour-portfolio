894ae077a3c6e40f9805312d0855362a35c5f684
Suraj Rathour <surajrathour0124@gmail.com>
2023-08-20 16:11:17 +0530

    Basic UI completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+// Toggle icon navebar ==================
+
+
+
+
+// scroll section 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
+
+window.onscroll = () =>{
+  section.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+          // active navbar links 
+          navLinks.forEach(links => {
+            links.classList,remove('active');
+            document.querySelector('header nav a[href*= ' + id + ' ]').classList.add('active');
+          })
+    }  
+  });
+  // sticky header 
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+}
+
 /* type test script animation */
 // banner text aniamtion
 
@@ -41,3 +72,5 @@ function clearTextFunc() {
 typeTextFunc();
 
 // banner text aniamtion end
+
+
